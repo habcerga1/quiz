95b4446699e1a610c112a98f7f4f7364b5f12df7
habcerga1 <habcerga1@gmail.com>
2022-08-09 18:15:02 +0300

    Add: Validator

--- Core/Validation/RegistrationDtoValidator.cs
+++ Core/Validation/RegistrationDtoValidator.cs
@@ -7,9 +7,26 @@ public class RegistrationDtoValidator : AbstractValidator<RegistrationDto>
 {
     public RegistrationDtoValidator()
     {
-        RuleFor(e => e.Email).NotEmpty();
-        RuleFor(e => e.Password).NotEmpty();
-        RuleFor(e => e.FirstName).NotEmpty();
-        RuleFor(e => e.LastName).NotEmpty();
+        RuleFor(e => e.Email)
+            .Cascade(CascadeMode.StopOnFirstFailure)
+            .NotEmpty()
+            .WithMessage("Email address is required")
+            .EmailAddress()
+            .WithMessage("A valid email is required")
+            .Length(6,40);
+        RuleFor(e => e.Password)
+            .Cascade(CascadeMode.StopOnFirstFailure)
+            .NotEmpty()
+            .WithMessage("Password is required, between 8 and 16 character")
+            .Length(8,16);
+        RuleFor(e => e.FirstName)
+            .Cascade(CascadeMode.StopOnFirstFailure)
+            .NotEmpty()
+            .WithMessage("FirstName is required")
+            .Length(1,20);
+        RuleFor(e => e.LastName)
+            .NotEmpty()
+            .WithMessage("LastName is required")
+            .Length(1,20);
     }
 }
